c4b362828f51af3bc84f7b8b2ec2b10b3b3127f1
martindustry <marcin.exn@gmail.com>
2022-08-06 17:29:22 +0200

    Change development database to SQLite
    * Switch database settings from PostgreSQL to SQLite

--- foreigner/settings.py
+++ foreigner/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'foreigner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
