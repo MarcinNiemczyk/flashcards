b2fd28c4187eb6d18f41a603a12b5937aeb53bef
martindustry <marcin.exn@gmail.com>
2022-09-04 12:18:45 +0200

    Add randomize setting
    * Generate random order to flashcards using Fisher-Yates algorithm
    
    * Remember order in local storage

--- flashcards/static/flashcards/learn.js
+++ flashcards/static/flashcards/learn.js
@@ -1,5 +1,8 @@
 const flashcards = document.querySelectorAll('.game-flashcard');
 
+const settings = loadSettings();
+let random = settings['random'];
+let order = settings['order'];
 let currentFlashcardIndex = loadIndex();
 loadFlashcard(currentFlashcardIndex);
 
@@ -26,33 +29,27 @@ document.getElementById('nextButton').onclick = () => {
    if (currentFlashcardIndex < flashcards.length - 1) {
       currentFlashcardIndex++;
       loadFlashcard(currentFlashcardIndex);
-
-      // Let the user start from scratch after learning from all flashcards
-      if (currentFlashcardIndex === flashcards.length - 1) {
-         localStorage.setItem('index', '0');
-      } else {
-         localStorage.setItem('index', currentFlashcardIndex);
-      }
+      localStorage.setItem('index', currentFlashcardIndex);
    }
 }
 
 document.getElementById('resetProgress').onclick = () => {
-   localStorage.setItem('index', '0');
-   currentFlashcardIndex = 0;
-   loadFlashcard(0);
+   resetFlashcards();
+}
+
+document.getElementById('saveSettings').onclick = () => {
+   if (document.getElementById('randomizeButton').checked) {
+      updateRandomizeValue(true);
+   } else {
+      updateRandomizeValue(false);
+   }
 }
 
 function loadIndex() {
    let index;
    if (localStorage.hasOwnProperty('index')) {
       index = parseInt(localStorage.getItem('index'));
-      // Ensure index is correct
-      try {
-         flashcards[index].classList.contains('active');
-      } catch(err) {
-         localStorage.removeItem('index');
-         return loadFlashcard(flashcards);
-      }
+      flashcards[index].classList.contains('active');
    } else {
       localStorage.setItem('index', '0');
       index = 0;
@@ -60,14 +57,67 @@ function loadIndex() {
    return index;
 }
 
-function loadFlashcard(index) {
-   resetFlashcards();
-   flashcards[index].classList.add('active');
+function loadSettings() {
+   let random;
+   let order;
+   if (localStorage.getItem('random') === 'true') {
+      random = Boolean(localStorage.getItem('random'));
+      document.getElementById('randomizeButton').checked = true;
+   } else {
+      localStorage.setItem('random', 'false');
+      random = false;
+   }
+   if (localStorage.hasOwnProperty('order')) {
+      order = JSON.parse(localStorage.getItem('order'));
+   } else {
+      order = setOrder();
+   }
+   return {'random': random, 'order': order}
 }
 
-function resetFlashcards() {
+function loadFlashcard(index) {
    flashcards.forEach(flashcard => {
       flashcard.style.transform = null;
       flashcard.classList.remove('active');
    });
+   flashcards[order[index]].classList.add('active');
+}
+
+function resetFlashcards() {
+   localStorage.setItem('index', '0');
+   currentFlashcardIndex = 0;
+   loadFlashcard(0);
+}
+
+function updateRandomizeValue(value) {
+   random = value;
+   localStorage.setItem('random', value);
+   if (value === true) {
+      order = shuffle(order);
+   } else {
+      order = setOrder();
+   }
+   resetFlashcards();
+}
+
+function setOrder() {
+   let order = [];
+   for (let i = 0; i < flashcards.length; i++) {
+      order.push(i);
+   }
+   localStorage.setItem('order', JSON.stringify(order));
+   return order;
+}
+
+function shuffle(array) {
+   // Fisher-Yates shuffle algorithm
+   let currentIndex = array.length, randomIndex;
+
+   while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+   }
+   localStorage.setItem('order', JSON.stringify(array));
+   return array;
 }
