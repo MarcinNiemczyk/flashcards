d9b8b3530aa92c0ff7359f721876d3b984e1494f
martindustry <marcin.exn@gmail.com>
2022-09-14 08:56:49 +0200

    Setup AWS S3 file storage

--- foreigner/settings.py
+++ foreigner/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -145,3 +146,12 @@ LOGOUT_REDIRECT_URL = 'login'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_REGION_NAME = 'eu-central-1'
+AWS_S3_FILE_OVERWRITE = False
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- users/models.py
+++ users/models.py
@@ -1,18 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from PIL import Image
 
 
 class User(AbstractUser):
     image = models.ImageField(default='default.jpg', upload_to='user_images')
-
-    def save(self, *args, **kwargs):
-        """Overwrite save method to resize every profile image"""
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-
-        if img.height > 128 or img.width > 128:
-            output_size = (128, 128)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
