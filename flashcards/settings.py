d3bcbc93e62872efc3476a4732ff427d7890ab60
martindustry <marcin.exn@gmail.com>
2022-11-13 16:24:32 +0100

    build: prepare project

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-mlrq)oh%%8g7)gf8jsc$(zk1^2kxkfl1tofh%a-$x7ip25lq29"
+SECRET_KEY = (
+    "django-insecure-mlrq)oh%%8g7)gf8jsc$(zk1^2kxkfl1tofh%a-$x7ip25lq29"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "backend.apps.BackendConfig",
 ]
 
 MIDDLEWARE = [
