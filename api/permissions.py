1207ab16317c89b11bb5f72b516224d12fa7e28a
martindustry <marcin.exn@gmail.com>
2022-12-09 21:58:25 +0100

    fix: change get_object_or_404 to rest framework implementation

--- api/permissions.py
+++ api/permissions.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import BasePermission
 
 from api.models import Deck
