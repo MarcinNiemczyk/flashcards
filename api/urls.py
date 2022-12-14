582ecf44a9275736a90b0d4db5bee0aac4e16fe6
martindustry <marcin.exn@gmail.com>
2022-12-14 12:07:33 +0100

    fix: specify typing in urls pk

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from api.views import BoxListView, DeckDetailView, DeckListView
 
 urlpatterns = [
     path("decks/", DeckListView.as_view(), name="deck-list"),
-    path("decks/<pk>", DeckDetailView.as_view(), name="deck-detail"),
+    path("decks/<int:pk>", DeckDetailView.as_view(), name="deck-detail"),
     path("decks/<int:deck_id>/boxes/", BoxListView.as_view(), name="box-list"),
 ]
