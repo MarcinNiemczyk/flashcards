a854388fd4b61ffd547135dda061c4e7ac6dd1e0
martindustry <marcin.exn@gmail.com>
2023-01-01 12:09:08 +0100

    refactor: compartmentalize code
    * Move signals logic to business logic in utils.py
    
    * Compartmentalize move card functions

--- api/signals.py
+++ api/signals.py
@@ -1,28 +1,13 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from api.models import Box, Card, Deck
+from api.models import Deck
+from api.utils import add_boxes_in_deck, reduce_boxes_in_deck
 
 
 @receiver(post_save, sender=Deck)
 def generate_boxes(sender, instance, created, **kwargs):
-    deck = instance
     if created:
-        for i in range(1, deck.box_amount + 1):
-            Box.objects.create(number_of=i, deck=deck)
+        add_boxes_in_deck(deck=instance)
     else:
-        current_boxes = Box.objects.filter(deck=deck).count()
-        new_box_amount = deck.box_amount
-
-        # Adding boxes to existing deck
-        if new_box_amount > current_boxes:
-            for i in range(current_boxes, new_box_amount):
-                Box.objects.create(number_of=i + 1, deck=deck)
-
-        # Reducing boxes in existing deck
-        elif new_box_amount < current_boxes:
-            new_last_box = Box.objects.get(number_of=new_box_amount, deck=deck)
-            for i in range(current_boxes, new_box_amount, -1):
-                box = Box.objects.get(number_of=i, deck=deck)
-                Card.objects.filter(box=box).update(box=new_last_box)
-                box.delete()
+        reduce_boxes_in_deck(deck=instance)
--- api/utils.py
+++ api/utils.py
@@ -36,26 +36,51 @@ def get_card_detail_absolute_url(request, card_id):
     return request.build_absolute_uri(url)
 
 
+def add_boxes_in_deck(deck):
+    for i in range(1, deck.box_amount + 1):
+        Box.objects.create(number_of=i, deck=deck)
+
+
+def reduce_boxes_in_deck(deck):
+    current_boxes = Box.objects.filter(deck=deck).count()
+    new_box_amount = deck.box_amount
+
+    # Adding boxes to existing deck
+    if new_box_amount > current_boxes:
+        for i in range(current_boxes, new_box_amount):
+            Box.objects.create(number_of=i + 1, deck=deck)
+
+    # Reducing boxes in existing deck
+    elif new_box_amount < current_boxes:
+        new_last_box = Box.objects.get(number_of=new_box_amount, deck=deck)
+        for i in range(current_boxes, new_box_amount, -1):
+            box = Box.objects.get(number_of=i, deck=deck)
+            Card.objects.filter(box=box).update(box=new_last_box)
+            box.delete()
+
+
 def move_card_to_next_box(card_id):
     card = Card.objects.get(pk=card_id)
     if card.deck.box_amount > card.box.number_of:
         new_box = Box.objects.get(
             deck=card.deck, number_of=card.box.number_of + 1
         )
-        card.box = new_box
-        delay_card(card)
-        card.save()
+        __move_card(card, new_box)
 
 
 def move_card_to_first_box(card_id):
     card = Card.objects.get(pk=card_id)
     new_box = Box.objects.get(deck=card.deck, number_of=1)
-    card.box = new_box
-    delay_card(card)
+    __move_card(card, new_box)
+
+
+def __move_card(card, box):
+    card.box = box
+    __delay_card(card)
     card.save()
 
 
-def delay_card(card):
+def __delay_card(card):
     """Hide card to the start of nth day"""
 
     card.active = False
